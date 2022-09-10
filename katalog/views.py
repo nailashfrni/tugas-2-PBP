9ad660b6d408969b2928aea8e43f9c5f3427a973
nailashafrni <nailashafirni2003@gmail.com>
2022-09-10 22:28:54 +0700

    Django App MVT Commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+katalog = CatalogItem.objects.all()
+context = {
+    "name": "Naila Shafirni Hidayat",
+    "id": 2106638324,
+    "katalog":katalog,
+}
+
+def show_katalog(request):
+    return render(request, "katalog.html", context)
