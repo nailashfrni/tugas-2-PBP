fc969db994fee4864df8276c385455d0836f27d7
nailashafrni <nailashafirni2003@gmail.com>
2022-09-22 10:27:53 +0700

    Mengerjakan tugas bonus

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -7,10 +7,22 @@ from mywatchlist.models import MyWatchList
 # Create your views here.
 def show_watchlist(request):
     watchlist = MyWatchList.objects.all()
+
+    num_of_watched = 0
+    for movie in watchlist:
+        if movie.watched == 'Yes':
+            num_of_watched += 1
+
+    if num_of_watched >= (len(watchlist) - num_of_watched):
+        message = "Selamat, kamu sudah banyak menonton!"
+    else:
+        message = "Wah, kamu masih sedikit menonton!"
+
     context = {
         "name": "Naila Shafirni Hidayat",
         "id": 2106638324,
         "watchlist": watchlist,
+        "message": message,
     }
     return render(request, "watchlist.html", context)
 
